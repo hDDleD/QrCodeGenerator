0adc97c3649ac694d69ff67338ec06a9b1b088c1
Teemu Hokkanen <hokkanen.teemu@outlook.com>
2015-10-25 22:35:15 +0200

    Added the whole project

---
+++ QrCodeGenerator/GetBrowserUrl.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using System.Windows;
+using NDde.Client;
+using System.Runtime.InteropServices;
+
+
+namespace QrCodeGenerator
+{
+    class GetBrowserUrl
+    {
+        public static string GetUrl(string browser) 
+        {
+            try
+            {
+                DdeClient dde = new DdeClient(browser, "WWW_GetWindowInfo"); // Using DDE-lib to connect and get information from the firefox browser
+                dde.Connect();
+                string url = dde.Request("URL", int.MaxValue);
+                string[] text = url.Split(new string[] { "\",\"" }, StringSplitOptions.RemoveEmptyEntries);
+                dde.Disconnect();
+                return text[0].Substring(1);
+            }
+            catch
+            {
+                MessageBox.Show("Is your firefox open?", "Error"); // If firefox isnt open
+                return null;
+            }
+        }
+
+        public static Bitmap GenerateQR(int width, int height, string text) // http://www.fluxbytes.com/csharp/how-to-generate-qr-barcodes-in-c/
+        {
+            var bw = new ZXing.BarcodeWriter();
+            var encOptions = new ZXing.Common.EncodingOptions() { Width = width, Height = height, Margin = 0 };
+            bw.Options = encOptions;
+            bw.Format = ZXing.BarcodeFormat.QR_CODE;
+            var result = new Bitmap(bw.Write(text)); // Create Bitmap with ZXing
+            return result;
+        }
+        public static BitmapImage BitmapToImageSource(Bitmap bitmap) // Converts Bitmap to ImageSource
+        {
+            using (MemoryStream memory = new MemoryStream())
+            {
+                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                memory.Position = 0;
+                BitmapImage bitmapimage = new BitmapImage();
+                bitmapimage.BeginInit();
+                bitmapimage.StreamSource = memory;
+                bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapimage.EndInit();
+
+                return bitmapimage;
+            }
+        }
+    }
+}
---
+++ QrCodeGenerator/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ZXing;
+using ZXing.QrCode;
+using ZXing.QrCode.Internal;
+
+namespace QrCodeGenerator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Left = 0;
+            this.Top = SystemParameters.PrimaryScreenHeight - this.Height - 40; // Set window to bottom left corner of screen
+            Bitmap bm = GetBrowserUrl.GenerateQR(200, 200, "www.test.com");  // Generate QRcode with ZXing
+            QR.Source = GetBrowserUrl.BitmapToImageSource(bm); // Convert Bitmap to ImageSource and set it to the programs image
+            QRText.Text = "www.test.com"; 
+        }
+
+        private void Firefox_Button_Click(object sender, RoutedEventArgs e) // Clicking the firefox buttons retrieves url from firefox browser and generates QRcode from that with ZXing
+        {
+            string url = GetBrowserUrl.GetUrl("firefox");
+            if(url != null)
+            {
+                Bitmap bm = GetBrowserUrl.GenerateQR(200, 200, url);
+                QR.Source = GetBrowserUrl.BitmapToImageSource(bm);
+                QRText.Text = url;
+            }
+        }
+        private void Text_Button_Click(object sender, RoutedEventArgs e) // Clicking the other buttons creates a QRCode from textbox string, if textbox is empty there is an error box
+        {
+            if(BoxText.Text.Length > 0)
+            {
+                Bitmap bm = GetBrowserUrl.GenerateQR(200, 200, BoxText.Text);
+                QR.Source = GetBrowserUrl.BitmapToImageSource(bm);
+                QRText.Text = BoxText.Text;
+                BoxText.Text = null;
+            }
+            else
+            {
+                MessageBox.Show("No text in textbox", "Error");
+            }
+        }
+
+        private void BoxText_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Enter)
+            {
+                if (BoxText.Text.Length > 0)
+                {
+                    Bitmap bm = GetBrowserUrl.GenerateQR(200, 200, BoxText.Text);
+                    QR.Source = GetBrowserUrl.BitmapToImageSource(bm);
+                    QRText.Text = BoxText.Text;
+                    BoxText.Text = null;
+                }
+                else
+                {
+                    MessageBox.Show("No text in textbox", "Error");
+                }
+            }
+        }
+            
+    }
+}
